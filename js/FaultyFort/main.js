dae637aefe1d239f154baf86cf416dbfc11fa90f
Ashley <ashley.pergoliti@gmail.com>
2024-10-27 11:15:35 +1000

    Added fort, enemy spawn circles, middle lane. removed test code comments

--- js/FaultyFort/main.js
+++ js/FaultyFort/main.js
@@ -3,23 +3,9 @@ $.use(update);
 
 //$.debug = true;
 
-/*
-const redBall = $.makeCircleCollider(10, 200, 50);
-redBall.speed = 25;
-redBall.direction = 90;
-redBall.friction = 0;
-redBall.fill = "red";
-redBall.mass=5;
-
-const squares = $.makeGroup();
-for (let i = 0; i < 100; i++) {
-    const y_offset = i * 20;
-    const square = $.makeBoxCollider($.w - 20, 20 + y_offset, 20, 20);
-    square.velocity.x = -20;
-    square.friction = 0;
-    squares.push(square);
-}
-*/
+//fort square
+const square = $.makeBoxCollider($.w - 20, $.h/2, 100, 100);
+square.fill = "#fcf403";
 
 //setup the game, only called on frame 0
 function setup(){
@@ -34,13 +20,23 @@ function update() {
         setup();
     }
 
-    /*if(redBall.collides(squares)){
-        console.log("1. ball hit squares",redBall.collides(squares)); 
-        console.log("speed is",redBall.speed);
-    }
-    if(squares.collides(redBall)){
-        console.log("2. squares hit balls",redBall.collides(squares));
-    }
-    redBall.draw();
-    squares.draw();*/
+    square.draw();
+
+    $.colour.stroke = "#000000";
+    $.colour.fill = "#ff0000";
+
+    //enemy spawn1 top
+    $.shape.oval(300,40,40,40,);
+
+    //enemy spawn2 middle
+    $.shape.oval(40,$.h/2,40,40,);
+
+    //middle 2 lanes
+    $.colour.stroke = "#ff0000";
+    $.shape.line(70,($.h/2)-40,($.w/2)+70,($.h/2)-40)
+    $.shape.line(70,($.h/2)+40,($.w/2)+70,($.h/2)+40)
+
+    $.colour.stroke = "#000000";
+    //enemy spawn3 top
+    $.shape.oval(300,$.h-40,40,40,);
 }
